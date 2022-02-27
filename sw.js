1e28d1143f4de736dc491c1d067041eda12f4458
Paul Tiarks <paultiarks@gmail.com>
2022-02-27 13:54:53 -0600

    Add index and SW and test content

---
+++ sw.js
@@ -0,0 +1,17 @@
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/SWTest/',                     // If you have separate JS/CSS files,
+  '/SWTest/index.html'            // add path to those files here
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url);
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  console.log("activate");
+})
